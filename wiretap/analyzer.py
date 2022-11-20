c61a18ced1c73ae0ecfc24245c65b28f9a070225
SudoOmbro <10577777@polimi.it>
2022-11-20 17:08:35 +0100

    improved analyzer

--- wiretap/analyzer.py
+++ wiretap/analyzer.py
@@ -1,56 +1,41 @@
+import re
 import threading
-from enum import Enum
-from functools import cache
-from typing import Any, List, Callable
+from typing import List, Callable, Any
 
 from wiretap.utils import Message
 
 
-class FilterPredicate(Enum):
-    EQUALS = 0
-    IN = 1
-    LESS_THAN = 2
-    LESS_OR_EQUAL_THAN = 3
-
-
-class ActionFilter:
-    """
-    filter messages by metadata (message_id, chat_id, chat_type, timestamp, sender_id).
-
-    Cache results to speed up filtering
-    """
-
-    def __init__(self, target_attribute: str, predicate: FilterPredicate, param: Any, negated: bool):
-        self.target_attribute = target_attribute
-        self.predicate = predicate
-        self.param = param
-        self.negated = negated
-
-    @cache
-    def evaluate(self, value: Any):
-        result: bool = False
-        match self.predicate:
-            case FilterPredicate.EQUALS:
-                result = value == self.param
-            case FilterPredicate.IN:
-                result = value in self.param
-            case FilterPredicate.LESS_THAN:
-                result = value < self.param
-            case FilterPredicate.LESS_OR_EQUAL_THAN:
-                result = value <= self.param
-        return not result if self.negated else result
-
-    def apply(self, message: Message):
-        return self.evaluate(message.__dict__[self.target_attribute])
-
-
 class AnalyzerAction:
 
-    def __init__(self, filters: List[ActionFilter], regex: str or None, action: Callable):
-        pass
+    def __init__(self, regex: str or None, lower: bool,  action: Callable[[Message], Any]):
+        self.regex = regex
+        self.action = action
+        if regex:
+            self.run = self.run_with_regex_lower if lower else self.run_with_regex
+        else:
+            self.run = self.run_no_regex
+
+    def run_with_regex(self, message: Message):
+        if re.match(self.regex, message.text):
+            self.action(message)
+
+    def run_with_regex_lower(self, message: Message):
+        if re.match(self.regex, message.lower_text):
+            self.action(message)
+
+    def run_no_regex(self, message: Message):
+        self.action(message)
 
 
-class MessageAnalyzer:
+class GenericMessageAnalyzer:
 
     def __init__(self):
         self.lock = threading.Lock()
+
+    def filter_actions(self, message: Message) -> List[AnalyzerAction]:
+        raise NotImplemented
+
+    def analyze(self, message: Message):
+        actions = self.filter_actions(message)
+        for action in actions:
+            action.run(message)
