6dc4d0333e5eb8d7870efa3938a0edb630791f4a
SudoOmbro <ombroso1000@gmail.com>
2022-12-24 15:01:26 +0100

    improved entity handling

--- wiretap/utils.py
+++ wiretap/utils.py
@@ -1,11 +1,22 @@
 import json
-from typing import Dict, Tuple
-
+from typing import Dict, Tuple, List, Union
 
 UPDATE_TYPE_CONTAINERS_MAP: Dict[str, Tuple[str, str]] = {
     "message": ("text", "from"),
     "channel_post": ("text", "sender_chat")
 }
+
+
+class MessageEntity:
+
+    def __init__(self, entity_dictionary: dict, encoded_text: bytes):
+        # entity offsets need to be converted from utf-16 to utf-8
+        self.text: str = encoded_text[
+                         entity_dictionary["offset"] * 2:
+                         (entity_dictionary["offset"] + entity_dictionary["length"]) * 2]\
+            .decode('utf-16-le')
+        self.type: str = entity_dictionary["type"]
+        self.url: Union[str, None] = entity_dictionary.get("url", None)
 
 
 class Message:
@@ -33,6 +44,11 @@ class Message:
         self.text: str = message_dict[container][data_container]
         self.sender_id: int = message_dict[container][sender_id_container]["id"]
         self.lower_text: str = self.text.lower()
+        self.entities: Union[List[MessageEntity], None] = []
+        # build entities list not up to api spec for ease of use reason
+        encoded_text: bytes = self.text.encode('utf-16-le')
+        for entity_dict in message_dict[container].get("entities", []):
+            self.entities.append(MessageEntity(entity_dict, encoded_text))
 
     def __str__(self):
         return json.dumps(self.__dict__)
